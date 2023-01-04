8635b8b7edf6fe0751938a1994820d46e720c031
caiobga99 <caiobga99@gmail.com>
2023-01-04 16:08:42 -0300

    exercicio dia 4 finalizado

---
+++ Month-01/Week-01/day-04/day-04-progress.js
@@ -0,0 +1,23 @@
+class Progress {
+  constructor(days) {
+    this.days = days;
+  }
+
+  wasDayProgressed() {
+    let progress = 0;
+    for (let i = 0; i < this.days.length; i++) {
+      if (this.days[i] < this.days[i + 1]) {
+        progress++;
+      }
+    }
+    console.log(`Ocorreu progresso em ${progress} dias`);
+  }
+}
+
+const test1 = [3, 4, 1, 2]; //➞ 2
+const test2 = [10, 11, 12, 9, 10]; //➞ 3
+const test3 = [6, 5, 4, 3, 2, 9]; //➞ 1
+const test4 = [9, 9]; //➞ 0
+
+const ProgressDay = new Progress(test4);
+ProgressDay.wasDayProgressed();
