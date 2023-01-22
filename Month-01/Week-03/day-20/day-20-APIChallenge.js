b5f62d3126f2383bf7151a765f3160afb4eda9a0
caiobga99 <caiobga99@gmail.com>
2023-01-22 17:42:09 -0300

    day 20 finished

---
+++ Month-01/Week-03/day-20/day-20-APIChallenge.js
@@ -0,0 +1,31 @@
+const apiKey = "66e97ebe08fb974555e8057851b0bd0d";
+const buttonSearch = document.querySelector("[data-button-search]");
+const res = document.querySelector("[data-result]");
+
+buttonSearch.addEventListener("click", async () => {
+  let city = document.querySelector("[data-city]").value;
+  res.innerHTML = "Loading";
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`
+  )
+    .then((Response) => Response.json())
+    .then((data) => {
+      if (
+        data.sys.country === "BR" ||
+        data.sys.country === "US" ||
+        data.sys.country === "CA"
+      ) {
+        res.innerHTML = `${data.name}-${data.sys.country}<br>Latitude: ${
+          data.coord.lat
+        }<br>Longitude: ${data.coord.lon}<br><br>Tempo: ${
+          parseInt(data.main.temp) + "ºC"
+        }<br>${data.weather[0].description}`;
+      } else {
+        res.innerHTML = "this city does not belong to the valid countries!";
+      }
+      console.log(data);
+    })
+    .catch((err) => {
+      res.innerHTML = "this city does not exist!";
+    });
+});
